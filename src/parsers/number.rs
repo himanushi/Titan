f8f9896228c9b7a16a0aba664cbc17d87db87e84
himanushi <himanushi@users.noreply.github.com>
2023-02-13 20:12:14 +0900

    test

--- src/parsers/number.rs
+++ src/parsers/number.rs
@@ -13,3 +13,39 @@ pub fn parse_number(input: &str) -> IResult<&str, Expr> {
         Expr::Num(s.parse().unwrap())
     })(input)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_number_with_positive_integer() {
+        let input = "123";
+        let expected_output = Ok(("", Expr::Num(123.0)));
+        let output = parse_number(input);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn test_parse_number_with_negative_integer() {
+        let input = "-123";
+        let expected_output = Ok(("", Expr::Num(-123.0)));
+        let output = parse_number(input);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn test_parse_number_with_positive_float() {
+        let input = "1.23";
+        let expected_output = Ok(("", Expr::Num(1.23)));
+        let output = parse_number(input);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn test_parse_number_with_negative_float() {
+        let input = "-1.23";
+        let expected_output = Ok(("", Expr::Num(-1.23)));
+        let output = parse_number(input);
+        assert_eq!(output, expected_output);
+    }
+}
