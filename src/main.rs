8b174a8a1c36ba0e431ac319712fa6476f53414f
himanushi <himanushi@users.noreply.github.com>
2023-02-13 19:37:33 +0900

    cargo
    https://github.com/himanushi/Titan/issues/13#issuecomment-1427698882

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let input = "1 + 2 * 3 - 4 / 2";
 
     // パーサーの実行
-    let (rest, result) = parsers::expression::parse_expression(input).unwrap();
+    let (rest, result) = parsers::expression::parse_expression(&input).unwrap();
 
     // パース結果の表示
     println!("Parsed successfully: {:?}", result);
