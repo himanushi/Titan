e80b8c101ac4a27222fafa87505227159c59ad92
himanushi <himanushi@users.noreply.github.com>
2023-02-14 08:59:22 +0900

    lint

--- src/parsers/factor.rs
+++ src/parsers/factor.rs
@@ -2,11 +2,23 @@ use crate::parsers::expr::Expr;
 use crate::parsers::expression::parse_expression;
 use crate::parsers::number::parse_number;
 
-use nom::{branch::alt, character::complete::char, sequence::delimited, IResult};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, space0},
+    combinator::{map, value},
+    multi::fold_many0,
+    sequence::{delimited, pair},
+    IResult,
+};
 
 pub fn parse_factor(input: &str) -> IResult<&str, Expr> {
     alt((
-        delimited(char('('), parse_expression, char(')')),
+        delimited(
+            space0,
+            delimited(char('('), parse_expression, char(')')),
+            space0,
+        ),
         parse_number,
     ))(input)
 }
--- src/parsers/term.rs
+++ src/parsers/term.rs
@@ -1,7 +1,15 @@
 use crate::parsers::expr::Expr;
 use crate::parsers::factor::parse_factor;
 
-use nom::{branch::alt, bytes::complete::tag, multi::fold_many0, sequence::pair, IResult};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, space0},
+    combinator::{map, value},
+    multi::fold_many0,
+    sequence::{delimited, pair},
+    IResult,
+};
 
 pub fn parse_term(input: &str) -> IResult<&str, Expr> {
     let (rest, lhs) = parse_factor(input)?;
