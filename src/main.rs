3aafbbe15d796e21e8da3f810a2ea5c952c7867c
himanushi <himanushi@users.noreply.github.com>
2023-02-13 14:03:19 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,39 @@ use nom::{
     bytes::complete::tag,
     character::complete::{char, digit1, multispace0},
     combinator::{map, opt},
-    sequence::{delimited, pair, preceded, terminated, tuple},
+    multi::fold_many0,
+    sequence::{delimited, pair, preceded, terminated, tuple, recognize},
     IResult,
 };
 
-fn main() {
-    let input = std::env::args().nth(1).unwrap();
-
-    // パーサーの実行
-    let (rest, result) = parse_expression(&input).unwrap();
-
-    // パース結果の表示
-    println!("Parsed successfully: {:?}", result);
-    println!("Remaining input: {:?}", rest);
+// 式のAST（抽象構文木）を表すデータ型
+#[derive(Debug)]
+enum Expr {
+    Num(f64),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
 }
 
-// 数値リテラルをパースするパーサー
+// 数値をパースするパーサー
 fn parse_number(input: &str) -> IResult<&str, f64> {
     map(
         recognize(tuple((
             opt(alt((char('+'), char('-')))),
             alt((digit1, preceded(char('.'), digit1))),
-            opt(preceded(char('e'), alt((char('+'), char('-'))))),
-            opt(digit1),
+            opt(preceded(char('e'), opt(alt((char('+'), char('-')))))),
         ))),
         |s: &str| s.parse().unwrap(),
     )(input)
+}
+
+// 因子をパースするパーサー
+fn parse_factor(input: &str) -> IResult<&str, Expr> {
+    alt((
+        map(delimited(char('('), parse_expression, char(')')), |expr| expr),
+        map(parse_number, Expr::Num),
+    ))(input)
 }
 
 // 項をパースするパーサー
@@ -55,40 +62,25 @@ fn parse_term(input: &str) -> IResult<&str, Expr> {
 fn parse_expression(input: &str) -> IResult<&str, Expr> {
     let (rest, mut lhs) = parse_term(input)?;
     let (rest, _) = multispace0(rest)?;
-    loop {
-        if let Ok((rest, op)) = alt((tag("+"), tag("-")))(rest) {
-            let (rest, _) = multispace0(rest)?;
-            let (rest, rhs) = parse_term(rest)?;
-            lhs = match op {
-                "+" => Expr::Add(Box::new(lhs), Box::new(rhs)),
-                "-" => Expr::Sub(Box::new(lhs), Box::new(rhs)),
-                _ => unreachable!(),
-            };
-        } else {
-            break;
-        }
-    }
-    Ok((rest, lhs))
+    let (rest, result) = fold_many0(
+        pair(alt((tag("+"), tag("-"))), parse_term),
+        lhs,
+        |acc, (op, rhs)| match op {
+            "+" => Expr::Add(Box::new(acc), Box::new(rhs)),
+            "-" => Expr::Sub(Box::new(acc), Box::new(rhs)),
+            _ => unreachable!(),
+        },
+    )(rest)?;
+    Ok((rest, result))
 }
 
-// 因子をパースするパーサー
-fn parse_factor(input: &str) -> IResult<&str, Expr> {
-    alt((
-        map(parse_number, Expr::Number),
-        delimited(
-            multispace0,
-            delimited(tag("("), parse_expression, delimited(tag(")"), multispace0)),
-            multispace0,
-        ),
-    ))(input)
-}
+fn main() {
+    let input = "1 + 2 * 3 - 4 / 2";
 
-// 式を表すデータ型
-#[derive(Debug)]
-enum Expr {
-    Number(f64),
-    Add(Box<Expr>, Box<Expr>),
-    Sub(Box<Expr>, Box<Expr>),
-    Mul(Box<Expr>, Box<Expr>),
-    Div(Box<Expr>, Box<Expr>),
+    // パーサーの実行
+    let (rest, result) = parse_expression(&input).unwrap();
+
+    // パース結果の表示
+    println!("Parsed successfully: {:?}", result);
+    println!("Remaining input: {:?}", rest);
 }
