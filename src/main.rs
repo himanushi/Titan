b5f1f5467bc21e2dc8bd1604b7a0561350d1dc7e
himanushi <himanushi@users.noreply.github.com>
2023-02-18 15:59:29 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,22 @@ extern crate pest_derive;
 
 use pest::Parser;
 
+// パーサーを定義する
 #[derive(Parser)]
-#[grammar = "ident.pest"]
-struct IdentParser;
+#[grammar = "expression.pest"]
+struct ExpressionParser;
 
+// メイン関数を定義する
 fn main() {
-    let pairs = IdentParser::parse(Rule::ident_list, "a1 b2").unwrap_or_else(|e| panic!("{}", e));
+    // 式を取得する
+    let expression = std::env::args().nth(1).unwrap();
 
-    // Because ident_list is silent, the iterator will contain idents
+    // 式をパースする
+    let pairs =
+        ExpressionParser::parse(Rule::expression, &expression).unwrap_or_else(|e| panic!("{}", e));
+
+    // パースした結果を表示する
     for pair in pairs {
-        // A pair is a combination of the rule which matched and a span of input
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
-
-        // A pair can be converted to an iterator of the tokens which make it up:
-        for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
-                _ => unreachable!(),
-            };
-        }
+        println!("{:?}", pair);
     }
 }
