d81d90328b2c88e91ae6bb3c0c2bed78255a6f54
himanushi <himanushi@users.noreply.github.com>
2023-02-18 16:04:04 +0900

    add pest

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,19 @@ fn main() {
 
     // パースした結果を表示する
     for pair in pairs {
-        println!("{:?}", pair);
+        // A pair is a combination of the rule which matched and a span of input
+        println!("Rule:    {:?}", pair.as_rule());
+        println!("Span:    {:?}", pair.as_span());
+        println!("Text:    {}", pair.as_str());
+        println!("{}", "");
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
+                Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
+                _ => unreachable!(),
+            };
+        }
     }
 }
